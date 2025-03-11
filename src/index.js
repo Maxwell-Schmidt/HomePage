0333dc8b3bbd2e36c96df6b1b73e3284bb89b535
Maxwell-Schmidt <max.r.schmidt@icloud.com>
2025-03-11 08:51:20 -0500

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Structural/Home.jsx';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Home />
 )
