2b08fef5572caa3a6fbe0da72301f619a6aa109a
Max Schmidt <max.r.schmidt@icloud.com>
2025-03-21 22:16:07 -0500

    Updated Feature to Allow Mermaid Diagrams

--- src/Content/FeaturedPage.jsx
+++ src/Content/FeaturedPage.jsx
@@ -1,5 +1,9 @@
 import {useEffect,useState} from 'react';
 import ReactMarkdown from 'react-markdown';
+import rehypeRaw from "rehype-raw";
+import rehypeStringify from "rehype-stringify";
+import remarkMermaidPlugin from 'remark-mermaid-plugin';
+
 
 function FeaturedPage(){
 
@@ -19,7 +23,7 @@ return (
         <h1>Featured Project:</h1>
         <br></br>
         <div style={{backgroundColor:"white",border:""}}>
-            <ReactMarkdown class="ReactMarkdown">{feature}</ReactMarkdown>
+            <ReactMarkdown children={feature} remarkPlugins={[[remarkMermaidPlugin, { theme: "light" }],]} rehypePlugins={[rehypeRaw,rehypeStringify,]}/>
             <a href="https://github.com/Maxwell-Schmidt/HomeBarInventory" target="_blank" style={{fontSize: "1.5em", textDecoration:"none"}}>See more in the GitHub Repository</a>
         </div>
     </div>
